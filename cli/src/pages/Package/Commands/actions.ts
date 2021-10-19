7cff4b4462cce2af0318801d096778932263309b
gandarfh <joao.ferreira@housi.com.br>
2021-10-19 13:34:58 -0300

    chore: change base path into actionsReadme cli

--- cli/src/pages/Package/Commands/actions.ts
+++ cli/src/pages/Package/Commands/actions.ts
@@ -28,7 +28,7 @@ export const actionReadme = async (name: string) => {
   const basePath = '../../../../..'
   const path = pathLib.join(__dirname, `${basePath}/README.md`)
   const where =
-    'Theme   | [`Theme`](https://npmjs.com/package/@llama-ui/theme) | [![latest](https://img.shields.io/npm/v/@llama-ui/theme/latest.svg)](https://npmjs.com/package/@llama-ui/theme) | [![README](https://img.shields.io/badge/README--@llama-ui/theme.svg)](packages/Theme/README.md)'
+    '| React       | [`React`](https://npmjs.com/package/@llama-ui/react)              | [![latest](https://img.shields.io/npm/v/@llama-ui/react/latest.svg)](https://npmjs.com/package/@llama-ui/react)               | [![README](https://img.shields.io/badge/README--@llama-ui/react.svg)](packages/React/README.md)              |'
   const data =
     '%COMPONENT% | [`%COMPONENT%`](https://npmjs.com/package/@llama-ui/%PACKAGE%) | [![latest](https://img.shields.io/npm/v/@llama-ui/%PACKAGE%/latest.svg)](https://npmjs.com/package/@llama-ui/%PACKAGE%) | [![README](https://img.shields.io/badge/README--@llama-ui/%PACKAGE%.svg)](packages/%COMPONENT%/README.md)'
 
